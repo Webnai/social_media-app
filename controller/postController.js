56045686fe911d8bc8faafa2d16bb938f7f3efd8
webnai <wilbertboadzo144@gmail.com>
2023-11-03 12:32:15 +0000

    post service update

---
+++ controller/postController.js
@@ -0,0 +1,54 @@
+import PostService from '../services/postService.js';
+import NotificationService from '../services/notificationService.js';
+
+class PostController {
+    // function to handle create post
+    static async createPost(req, res, next) {
+        try {
+            const response = await PostService.createPost(req.body);
+            return res.status(201).json({message: 'post created successfully', response});
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    // function to handle get a post
+    static async getPost(req, res, next) {
+        try {
+            const response = await PostService.getPost(req.body);
+            return res.status(200).json({message: 'post retrieved successfully', response});
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    // function to handle get all posts
+    static async getAllPosts(req, res, next) {
+        try {
+            const response = await PostService.getAllPosts(req.body);
+            return res.status(200).json({message: 'posts retrieved successfully', response});
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    // function to handle update post
+    static async updatePost(req, res, next) {
+        try {
+            const response = await PostService.updatePost(req.body);
+            return res.status(200).json({message: 'post updated successfully', response});
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    // function to handle delete post
+    static async deletePost(req, res, next) {
+        try {
+            const response = await PostService.deletePost(req.body);
+            return res.status(200).json({message: 'post deleted successfully', response});
+        } catch (error) {
+            next(error);
+        }
+    }
+}
--- service/postService.js
+++ service/postService.js
@@ -66,4 +66,109 @@ class PostService {
             console.error({ message: "server error", error});
         }
     }
+
+    //function to handle get all posts
+    static async getAllPosts(req, res, next) {
+        try {
+            // find all posts
+            const posts = await Post.findAll({
+                include: User,
+                include: Like,
+                include: Comment,
+            });
+            if (!posts) {
+                return res.status(400).json({
+                    status: 'error',
+                    message: 'Posts not found',
+                });
+            }
+            // return all posts
+            return res.status(200).json({
+                status: 'success',
+                message: 'posts retrieved successfully',
+                data: posts,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // function to handle update post
+    static async updatePost(req, res, next) {
+        try {
+            // check if post exists
+            const post = await Post.findOne({
+                where: {
+                    id: req.params.id,
+                },
+            });
+            if (!post) {
+                return res.status(400).json({
+                    status: 'error',
+                    message: 'Post not found',
+                });
+            }
+            // update post
+            const updatedPost = await Post.update({
+                title: req.body.title,
+                content: req.body.content,
+            }, {
+                where: {
+                    id: req.params.id,
+                },
+            });
+            if (!updatedPost) {
+                return res.status(400).json({
+                    status: 'error',
+                    message: 'Post not updated',
+                });
+            }
+            // return updated post
+            return res.status(200).json({
+                status: 'success',
+                message: 'post updated successfully',
+                data: updatedPost,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    //function to handle delete post
+    static async deletePost(req, res, next) {
+        try {
+            // check if post exists
+            const post = await Post.findOne({
+                where: {
+                    id: req.params.id,
+                },
+            });
+            if (!post) {
+                return res.status(400).json({
+                    status: 'error',
+                    message: 'Post not found',
+                });
+            }
+            // delete post
+            const deletedPost = await Post.destroy({
+                where: {
+                    id: req.params.id,
+                },
+            });
+            if (!deletedPost) {
+                return res.status(400).json({
+                    status: 'error',
+                    message: 'Post not deleted',
+                });
+            }
+            // return deleted post
+            return res.status(200).json({
+                status: 'success',
+                message: 'post deleted successfully',
+                data: deletedPost,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
